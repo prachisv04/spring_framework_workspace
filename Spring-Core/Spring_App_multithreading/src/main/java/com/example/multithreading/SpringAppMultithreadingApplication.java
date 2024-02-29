f6787ac3c43ae5a9ca1d9b7fa586abf2345a58f4
prachisv04 <prachisvavhal@gmail.com>
2024-02-29 19:52:58 +0530

    Multithreading

---
+++ Spring-Core/Spring_App_multithreading/src/main/java/com/example/multithreading/SpringAppMultithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.example.multithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppMultithreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppMultithreadingApplication.class, args);
+	}
+
+}
---
+++ Spring-Core/Spring_App_multithreading/src/test/java/com/example/Spring_App_multithreading/SpringAppMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_App_multithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppMultithreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
